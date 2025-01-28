9653c7dcb01979a06fa916e332fb09e2364e25bb
Tazwar Saif <tazwarsaif02@gmail.com>
2025-01-28 12:17:26 +0600

    right panel follow functionality added

--- frontend/src/componenets/common/RightPanel.jsx
+++ frontend/src/componenets/common/RightPanel.jsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import useFollow from "../../hooks/useFollow";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-
+import LoadingSpinner from "./LoadingSpinner";
 const RightPanel = () => {
   const { data: suggestedUsers, isLoading } = useQuery({
     queryKey: ["suggestedUsers"],
@@ -18,6 +19,7 @@ const RightPanel = () => {
       }
     },
   });
+  const { follow, isPending } = useFollow();
   if (suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>;
 
   return (
@@ -58,9 +60,16 @@ const RightPanel = () => {
                 <div>
                   <button
                     className="btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm"
-                    onClick={(e) => e.preventDefault()}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      follow(user._id);
+                    }}
                   >
-                    Follow
+                    {isPending ? (
+                      <LoadingSpinner size="sm"></LoadingSpinner>
+                    ) : (
+                      "Follow"
+                    )}
                   </button>
                 </div>
               </Link>
