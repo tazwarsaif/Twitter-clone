a554d31b85193aac9a09cc19773fa87a329947a3
Tazwar Saif <tazwarsaif02@gmail.com>
2025-01-28 14:35:03 +0600

    Comment feature added

--- frontend/src/componenets/common/Post.jsx
+++ frontend/src/componenets/common/Post.jsx
@@ -5,6 +5,7 @@ import { BiRepost } from "react-icons/bi";
 import { FaRegComment, FaRegHeart, FaTrash } from "react-icons/fa";
 import { FaRegBookmark } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { formatPostDate } from "../../utils/date";
 import LoadingSpinner from "./LoadingSpinner";
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
@@ -63,21 +64,48 @@ const Post = ({ post }) => {
       toast.error(error.message);
     },
   });
+  const { mutate: commentPost, isPending: isCommenting } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/posts/comment/${post._id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: comment }),
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Comment posted successfully");
+      setComment("");
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
   const postOwner = post.user;
   const isLiked = post.likes.includes(authUser._id);
 
   const isMyPost = authUser._id === post.user._id;
 
-  const formattedDate = "1h";
-
-  const isCommenting = false;
+  const formattedDate = formatPostDate(post.createdAt);
 
   const handleDeletePost = () => {
     deletePost();
   };
 
-  const handlePostComment = (e) => {
-    e.preventDefault();
+  const handlePostComment = () => {
+    commentPost();
   };
 
   const handleLikePost = () => {
